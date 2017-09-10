75559d37617b621c2277d776ca27547ca8d775f8
Evan Pratten <ewpratten@gmail.com>
2017-09-10 11:14:10 -0400

    Create index.php

---
+++ community/index.php
@@ -0,0 +1,86 @@
+<html lang="en"><head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, minimumscale=1.0, maximum-scale=1.0">
+
+	
+    <title>RetryLife Community</title>
+  
+<style>
+	body {
+		font-family: Roboto;
+		margin: 0;
+		padding: 0;
+		background-image: url("../img/Background.png");
+		background-size: 100% 100%;
+		
+	}
+	.rant-top-bar {
+		background-color:white;
+	}
+</style>
+    
+
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet" type="text/css">
+
+    <link href="./style.css" rel="stylesheet" type="text/css">
+    <link href="https://devrant.io/static/devrant/css/font-style.css?v=5" rel="stylesheet" type="text/css">
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://devrant.io/static/devrant/js/script.js?v=23"></script>
+	<script src="https://devrant.io/static/devrant/js/js.cookie-2.1.1.min.js"></script>
+    <script src="https://devrant.io/static/devrant/js/landing.js?v=5"></script>
+    
+    <script src="./json.js"></script>
+</head>
+<body >
+
+
+  
+
+	
+
+
+
+
+
+<div class="container">
+
+    <div class="interior-centered">
+        <div class="interior-content" style="min-height: 804px;">
+
+            <div class="body-col1">
+                <h1>RetryLife</h1>
+            </div>
+
+<div class="body-col2 page-feed">
+    
+    
+
+		<div class="rantlist-bg">
+	    <ul class="rantlist">
+
+	    	
+	    	<li class="rant-comment-row-widget" data-id="829770" data-type="rant">
+	    	<div class="rantlist-title-text">The Lost Warrior</div>
+	    	<img src="http://art.ngfiles.com/images/477000/477954_glitchop_the-lost-warrior-2.png" width="100%" alt="Card image cap">
+	    	</ul>
+	    	</div>
+    
+</div>
+<div class="clearfix"></div>
+</div>
+</div>
+
+
+
+
+
+</div>  <!-- end container -->
+
+
+
+</body></html>
