2e701743b8c2d47da444a4e05cba823293a49f47
Evan Pratten <ewpratten@gmail.com>
2017-09-11 20:00:07 -0400

    Update index.php

--- community/index.php
+++ community/index.php
@@ -31,8 +31,7 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
     <script src="https://devrant.io/static/devrant/js/script.js?v=23"></script>
-	<script src="https://devrant.io/static/devrant/js/js.cookie-2.1.1.min.js"></script>
-    <script src="https://devrant.io/static/devrant/js/landing.js?v=5"></script>
+
     <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     
 </head>
