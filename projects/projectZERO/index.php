10018e78d20b1cca702d037ab275b56077cfe7b7
Evan Pratten <ewpratten@gmail.com>
2017-11-19 16:44:04 -0500

    Update index.php

--- projects/projectZERO/index.php
+++ projects/projectZERO/index.php
@@ -2,6 +2,7 @@
 	<link href="./css/picnic.min.css" rel="stylesheet" media="all">
 	<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, inital-scale=1">
+	<link href="css/hover.css" rel="stylesheet" media="all">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 	<!-- <link href="./css/main.css" rel="stylesheet" media="all"> -->
 	<style>
@@ -47,6 +48,26 @@ footer {
   height: 100%;
   padding: 0px;
   margin: 0px;
+}
+.border {
+    border-color: white;
+    border-style: solid;
+    width: fit-content;
+    border-radius: 100%;
+}
+.ewp {
+    border-radius: 100%;
+}
+
+a.button.hvr-bob {
+    border-radius: 100%;
+    background-color: rgba(255, 255, 255, 0);
+}
+img.dri {
+    height: 20%;
+}
+a.abc {
+    color: #405c7d;
 }
 	</style>
 </head>
@@ -145,7 +166,7 @@ if($currentPage != "home"){
 
 <br>
 <br>
-<div style="height:30%">
+<div style="height:300">
 	<div id="gradient" />
 <!-- im a spacer -->
 
@@ -250,9 +271,84 @@ for($ix = 0; $ix <= 33; $ix++){
 		</div>
 </div>
 
+
+
+
+
+<div class="spacer"></div>
+
+
+
+
+<div class="flex one-0 three-600 demo">
+  <div class="two-third"><span>
+  	<img src="">
+  	<h1>(Image of living space)</h1>
+  </span></div>
+  <div><span>
+  	<article class="card"><header>
+    	<h3>Living Spaces</h3>
+  </header>
+  
+  <p>(add details about the apartments)</p>
+  
+  </article>
+  </span></div>
+  
+</div>
+
+<div class="spacer"></div>
+
+<div class="flex one-0 three-600 demo">
+	<div><span>
+  	<article class="card"><header>
+    	<h3>Office Spaces</h3>
+  </header>
+  
+  <p>(add details about the offices)</p>
+  
+  </article>
+  </span></div>
+  
+  <div class="two-third"><span>
+  	<img src="">
+  	<h1>(Image of office space)</h1>
+  </span></div>
+  
+  
+</div>
+
+
 <footer>
-<div width="100%"  style="background-color: #243447;height:250;">
-<h3>Website by: <a href="https://ewpratten.github.io">Ewpratten</a></h3>
+<div width="100%"  style="background-color: #243447;height:auto;">
+<!-- <h3>Website by: <a href="https://ewpratten.github.io">Ewpratten</a></h3> -->
+
+<!--
+<div class="dr" style="white-space: nowrap;width: 90%;float: left;">
+		<div class="ewp" style="display: inline-block;">
+			<a href="https://devrant.com/users/ewpratten" title="ewpratten" class="button hvr-bob">
+				<div class="border">
+			<img style="border-radius: 100%;" class="dri" src="https://avatars.devrant.com/v-18_c-3_b-1_g-m_9-1_1-6_16-8_3-3_8-4_7-4_5-3_12-6_17-2_6-34_10-9_2-47_11-4_18-4_4-3_19-3_20-5_21-2.jpg">
+			</div>
+			</a>
+			</div>
+			<div class="404" style="display: inline-block;">
+				<a href="https://devrant.com/users/desjna" title="desjna" class="button hvr-bob">
+					<div class="border">
+					<img style="border-radius: 100%;" class="dri" src="https://avatars.devrant.com/v-18_c-3_b-4_g-m_9-1_1-9_16-15_3-3_8-1_7-1_5-1_12-9_6-40_2-39_4-1.jpg">
+					</div>
+				</a>
+			</div>
+				
+        </div>
+        -->
+        
+
+<div class="credits">
+<p>Project by: <a title="My name is Evan" href="https://github.com/Ewpratten" style="color: white;">ewpratten</a>, Maya, Sarah, Nathan<a class="abc" title="Hey look! You can click me to make a donation!" href="http://retrylife.ca/donate" style="float:right;">Support the developer</a></p>
+</div>
+    </div>
+</div>
 </div>
 </footer>
 </body>
