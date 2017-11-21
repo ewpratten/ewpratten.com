9b1e3736f14ad29b654ea043a6b490a1273fb99a
Evan Pratten <ewpratten@gmail.com>
2017-11-21 12:59:25 -0500

    Update index.php

--- projects/projectZERO/index.php
+++ projects/projectZERO/index.php
@@ -1,3 +1,35 @@
+ <!--
+
+   _____           _           _   ____________ _____   ____
+  |  __ \         (_)         | | |___  /  ____|  __ \ / __ \
+  | |__) | __ ___  _  ___  ___| |_   / /| |__  | |__) | |  | |
+  |  ___/ '__/ _ \| |/ _ \/ __| __| / / |  __| |  _  /| |  | |
+  | |   | | | (_) | |  __/ (__| |_ / /__| |____| | \ \| |__| |
+  |_|   |_|  \___/| |\___|\___|\__/_____|______|_|  \_\\____/
+                 _/ |
+                |__/
+                
+  |---------------------------------------------------------|
+  |						Website Credits						|
+  |				Front / Back end: Evan Pratten				|
+  |					Artwork: Nathan Desjardins				|
+  |---------------------------------------------------------|
+  
+  Check out my main website:
+  
+  Http://retrylife.ca
+  
+  
+  -----
+  NOTES:
+  
+  There is a lot of half working code here.. so sory if you cant figure it out.
+  
+  I also was too lazy to comment the code.
+
+-->
+
+
 <head>
 	<link href="./css/picnic.min.css" rel="stylesheet" media="all">
 	<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
@@ -79,6 +111,17 @@ img.dri {
 a.abc {
     color: #405c7d;
 }
+.line .linev {
+	background-color:black;
+}
+.line {
+	height: 1;
+	width:100%;
+}
+.linev {
+	height:100%;
+	width:1;
+}
 	</style>
 </head>
 <body>
@@ -159,10 +202,17 @@ echo "Project ZERO"
   <label for="bmenub" class="success burger button">menu</label>
 
   <div class="menu">
-	<a href="#" class="button">Office Space</a>
-	<a href="#" class="button">Housing</a>
-	<a href="#" class="button">Design</a>
+  	
+  	<a href="https://twitter.com/ProjectZERO_nr?ref_src=twsrc%5Etfw" align="center" class="twitter-follow-button" data-show-count="false"><button>Twittter</button></a>
+  	
+  	<a href="https://www.instagram.com/projectzero_nr/" class="twitter-follow-button" data-show-count="false" align="center"><button>Instagram</button></a>
+  	
+	<a href="#office" class="button">Office Space</a>
+	<a href="#housing" class="button">Housing</a>
+	<a href="#design" class="button">Design</a>
 <?php
+$pagestate = $_GET['page'];
+if($pagestate == "")
 $currentPage = "home";
 if($currentPage != "home"){
     echo '<a href="./index.html" class="button">Home</a>';
@@ -207,7 +257,7 @@ for($ix = 0; $ix <= 33; $ix++){
 </div>
 <div class="spacer"></div>
 
-<div class="housing">
+<div class="housing" id="housing">
 	<div style="align:center:">
 		<h1 align="center">Housing</h1>
 		</div>
@@ -245,7 +295,7 @@ for($ix = 0; $ix <= 33; $ix++){
 
 <div class="spacer"></div>
 
-<div class="offices">
+<div class="offices" id="office">
 	<div style="align:center:">
 		<h1 align="center">Office Spaces</h1>
 		</div>
@@ -287,10 +337,157 @@ for($ix = 0; $ix <= 33; $ix++){
 
 <div class="spacer"></div>
 
+<!--
+?php
+/*
+Configuration Array
+ 
+explanation of each option can be seen here : https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
+ 
+user = the screen_name of the twitter user you wish to query
+count = the "maximum" number of items to be returned
+retweet = true or false to include retweets in the response
+entities = true or false
+exclude_replies = true or false to exclude replies
+contributor_details = true or false
+trim_user = true or false to trim extra user details
+ 
+*/
+ 
+$twitter = array(
+	"user" => "karlblessing",
+	"count" => "4",
+	"retweet" => "true",
+	"entities" => "true",
+	"exclude_replies" => "true",
+	"contributor_details" => "false",
+	"trim_user" => "false"
+);
+ 
+// a small function to convert "created at" time to [blank] minutes/hours/days ago
+ 
+function relativeTime($time)
+{
+    $delta = strtotime('+2 hours') - strtotime($time);
+    if ($delta < 2 * MINUTE) {
+        return "1 min ago";
+    }
+    if ($delta < 45 * MINUTE) {
+        return floor($delta / MINUTE) . " min ago";
+    }
+    if ($delta < 90 * MINUTE) {
+        return "1 hour ago";
+    }
+    if ($delta < 24 * HOUR) {
+        return floor($delta / HOUR) . " hours ago";
+    }
+    if ($delta < 48 * HOUR) {
+        return "yesterday";
+    }
+    if ($delta < 30 * DAY) {
+        return floor($delta / DAY) . " days ago";
+    }
+    if ($delta < 12 * MONTH) {
+        $months = floor($delta / DAY / 30);
+        return $months <= 1 ? "1 month ago" : $months . " months ago";
+    } else {
+        $years = floor($delta / DAY / 365);
+        return $years <= 1 ? "1 year ago" : $years . " years ago";
+    }
+}
+ 
+// prepare the array
+ 
+$twitter_feed = array();
+ 
+// form the API url for the request
+ 
+$api_url = "https://api.twitter.com/1/statuses/user_timeline/".$twitter['user'].
+	".json?include_entities=".$twitter['entities'].
+	"&include_rts=".$twitter['retweet'].
+	"&exclude_replies=".$twitter['exclude_replies'].
+	"&contributor_details=".$twitter['contributor_details'].
+	"&trim_user=".$twitter['trim_user'].
+	"&count=".$twitter['count'];
+ 
+// obtain the results
+ 
+$json = file_get_contents($api_url, true);
+ 
+// decode the json response as a PHP array
+ 
+$decode = json_decode($json, true);
+ 
+//check for error during the last decode
+if(json_last_error != JSON_ERROR_NONE) {
+	// http://www.php.net/manual/en/function.json-last-error.php
+	$twitter_feed[] = array('error' => "Unable to decode response");
+} elseif(isset($decode['errors'])) {
+	// just grabbing the first error listed
+	$twitter_feed[] = array('error' => $decode['errors'][0]['message']);
+} else {
+	// if no decode or twitter response errors then proceed.
+ 
+	foreach($decode as $tweet) {
+		// If you are including retweets, you may want to check the status
+		// as the main text is truncated as opposed to the original tweet
+ 
+		// If you used the trim_user option, the retweeted user screen name will not be avaialble
+ 
+		if (isset($tweet['retweeted_status'])) {
+			$tweet_text = "RT @{$tweet['retweeted_status']['user']['screen_name']}:
+			{$tweet['retweeted_status']['text']}";
+		} else {
+			$tweet_text = $tweet['text'];
+		}
+ 
+		$twitter_feed[] = array(
+			'text' => $tweet_text,
+			'created_at' => relativeTime($tweet['created_at']),
+			'link' => "http://twitter.com/".$twitter['user']."/status/".$tweet['id']
+		);
+ 
+		unset($tweet_text);
+	}
+}
+ 
+unset($decode, $json, $tweet);
+?
+-->
 
+<!--
+?php
+ 
+// in a later portion of your code or page you can break down the array like so:
+ 
+foreach($twitter_feed as $tweet) {
+	echo "<a href=\"{$tweet['link']}\" target=\"_blank\">{$tweet['text']}</a><br>{$tweet['created_at']}<br><br>";
+}
+ 
+?
+-->
+<!--
+ <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+ <a class="twitter-timeline" href="https://twitter.com/ProjectZERO_nr?ref_src=twsrc%5Etfw">Tweets by ProjectZERO_nr</a>
+-->
+<!--
+<div class="flex one three-600 demo">
+			<div><span><article class="card">
+<a href="https://twitter.com/ProjectZERO_nr?ref_src=twsrc%5Etfw" align="center" class="twitter-follow-button" data-show-count="false"><button>Follow @ProjectZERO on twittter</button></a>
+</article></span></div>
+<div><span><article class="card"><h1>Follow our social media to see new updates</h1></article></article></span></div>
+<div><span><article class="card">
+	<a href="https://www.instagram.com/projectzero_nr/" class="twitter-follow-button" data-show-count="false" align="center"><button>Follow @ProjectZERO on instagram</button></a>
+	
+	
+	
+	</article></article></span></div>
+	</div>
 
+<div class="spacer"></div>
+-->
 
-<div class="flex one-0 three-600 demo">
+<div class="flex one-0 three-600 demo" id="design">
   <div class="two-third"><span>
   	<img src="">
   	<h1>(Image of living space)</h1>
@@ -354,8 +551,8 @@ for($ix = 0; $ix <= 33; $ix++){
         -->
         
 
-<div class="credits">
-<p>Project by: <a title="My name is Evan" href="https://github.com/Ewpratten" style="color: white;">ewpratten</a>, Maya, Sarah, Nathan<a class="abc" title="Hey look! You can click me to make a donation!" href="http://retrylife.ca/donate" style="float:right;">Support the developer</a></p>
+<div class="credits" width="100%">
+<p>Project by: <a title="My name is Evan" href="https://github.com/Ewpratten" style="color: white;">ewpratten</a>, Maya, Sarah, Nathan<a class="abc" title="Hey look! You can click me to make a donation!" href="http://retrylife.ca/donate" style="float :right;">Support the developer</a></p>
 </div>
     </div>
 </div>
