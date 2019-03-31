14a3ce734375e37f50cc4a1a52bdb6543bd89411
Evan Pratten <ewpratten@gmail.com>
2019-03-31 19:41:05 -0400

    updated to new template

---
+++ js/console.js
@@ -0,0 +1,3 @@
+console.log("Welcome, traveler!")
+console.log("You have stumbled upon the mystical terminal. ")
+console.log("This website is based off a template I origonally made for hyperlisk.xyz")
