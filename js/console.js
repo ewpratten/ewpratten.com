17df736309489ecf862285ef95f6be6baf53ec1c
Evan Pratten <ewpratten@gmail.com>
2019-05-22 12:54:54 -0400

    Here we go again..

--- js/console.js
+++
@@ -1,3 +0,0 @@
-console.log("Welcome, traveler!")
-console.log("You have stumbled upon the mystical terminal. ")
-console.log("This website is based off a template I origonally made for hyperlisk.xyz")
