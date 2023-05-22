a86ef1ff936e1f98219d0c42e4255e88a4af9fd7
Evan Pratten <ewpratten@gmail.com>
2023-05-22 11:59:16 -0400

    add warnings

--- static/functions/_middleware.js
+++ static/functions/_middleware.js
@@ -1,5 +1,14 @@
 async function goat_counter_analytics(context) {
 
+
+    // We require some env vars to be set. If they are not, fail the request
+    if (!context.env.GOAT_COUNTER_API_KEY) {
+        return new Response('$GOAT_COUNTER_API_KEY is not set', { status: 500, headers: { 'Content-Type': 'text/plain' } });
+    }
+    if (!context.env.GOAT_COUNTER_SITE_CODE) {
+        return new Response('$GOAT_COUNTER_SITE_CODE is not set', { status: 500, headers: { 'Content-Type': 'text/plain' } });
+    }
+
     // Parse the request URL
     let url = new URL(context.request.url);
 
@@ -17,13 +26,16 @@ async function goat_counter_analytics(context) {
         ]
     };
 
+    // Count the goat
+    // await fetch()
+
     // Execute the rest of the request chain
     let response = await context.next();
 
     // For debugging, allow the requester to expose the body through a response header
     if (url.searchParams.get('goat-counter-debug') == 'true') {
         response.headers.set('X-GoatCounter-Payload', JSON.stringify(payload));
-        response.headers.set('X-GoatCounter-Api-Token', context.env.GOAT_COUNTER_API_TOKEN);
+        response.headers.set('X-GoatCounter-Api-Token', context.env.GOAT_COUNTER_API_KEY);
     }
 
     // Return the response
