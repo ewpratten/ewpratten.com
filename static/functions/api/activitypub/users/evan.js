25e1e13e6eaf613826357b73ef326ddb7b2c750b
Evan Pratten <evan@ewpratten.com>
2024-04-13 16:55:06 -0400

    tinkering with format

--- static/functions/api/activitypub/users/evan.js
+++ static/functions/api/activitypub/users/evan.js
@@ -1,9 +1,11 @@
 export function onRequest(context) {
     return new Response(
         JSON.stringify({
-            "@context": ["https://www.w3.org/ns/activitystreams", { "@language": "en-CA" }],
+            "@context": ["https://www.w3.org/ns/activitystreams","https://w3id.org/security/v1" ],
             "type": "Person",
+            "manuallyApprovesFollowers": true,
             "id": "https://ewpratten.com/api/activitypub/users/evan",
+            "outbox": "https://ewpratten.com/api/activitypub/inbox",
             "outbox": "https://ewpratten.com/api/activitypub/outbox",
             "preferredUsername": "evan",
             "name": "Evan Pratten",
@@ -12,11 +14,10 @@ export function onRequest(context) {
                 "https://ewpratten.com/images/pfp/2022/460x460.webp"
             ],
             "publicKey": {
-                "@context": "https://w3id.org/security/v1",
                 "@type": "Key",
                 "id": "https://ewpratten.com/api/activitypub/users/evan#main-key",
                 "owner": "https://ewpratten.com/api/activitypub/users/evan",
-                "publicKeyPem": "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJwe4jxrpiDx0vzqnoc+3Mja7Xz73/NxfDqG9Mu+k6Vs87N/+kV4BbsbJ/vtdYAg58+iMDmyRw48CzaXkPDgiCh3RZFc/8GniBSEucjt/QEiAitV48aykqWyXtln0hAmQrjoEeE9DRxS3eyF7FVE2GhkTz1YqBabOMpHA1uGOp7QIDAQAB"
+                "publicKeyPem": "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJwe4jxrpiDx0vzqnoc+3Mja7X\nz73/NxfDqG9Mu+k6Vs87N/+kV4BbsbJ/vtdYAg58+iMDmyRw48CzaXkPDgiCh3RZ\nFc/8GniBSEucjt/QEiAitV48aykqWyXtln0hAmQrjoEeE9DRxS3eyF7FVE2GhkTz\n1YqBabOMpHA1uGOp7QIDAQAB\n-----END PUBLIC KEY-----"
             }
         }),
         {
