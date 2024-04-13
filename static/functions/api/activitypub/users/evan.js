a733e2946345b4bd979fa802ac2ff7fecc2ad92b
Evan Pratten <evan@ewpratten.com>
2024-04-13 16:45:24 -0400

    Add a fake pubkey

--- static/functions/api/activitypub/users/evan.js
+++ static/functions/api/activitypub/users/evan.js
@@ -10,7 +10,14 @@ export function onRequest(context) {
             "summary": "",
             "icon": [
                 "https://ewpratten.com/images/pfp/2022/460x460.webp"
-            ]
+            ],
+            "publicKey": {
+                "@context": "https://w3id.org/security/v1",
+                "@type": "Key",
+                "id": "https://ewpratten.com/api/activitypub/users/evan#main-key",
+                "owner": "https://ewpratten.com/api/activitypub/users/evan",
+                "publicKeyPem": "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJwe4jxrpiDx0vzqnoc+3Mja7Xz73/NxfDqG9Mu+k6Vs87N/+kV4BbsbJ/vtdYAg58+iMDmyRw48CzaXkPDgiCh3RZFc/8GniBSEucjt/QEiAitV48aykqWyXtln0hAmQrjoEeE9DRxS3eyF7FVE2GhkTz1YqBabOMpHA1uGOp7QIDAQAB"
+            }
         }),
         {
             headers: {
