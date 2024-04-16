18cf46ef33e6bf0417776cc3f54dcd90dc0f4694
Evan Pratten <evan@ewpratten.com>
2024-04-16 11:25:05 -0400

    Update generation logic

--- static/functions/keys/ssh.js
+++ static/functions/keys/ssh.js
@@ -1,10 +1,18 @@
 const SSH_PUBKEYS = {
     devices: {
-        desktop: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBaSKoWYuR43fxRPy31P/X/2Ri2hYUZTjKiLBRDoa1F",
-        laptop: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEqeWe+pVp3rTsmHr9v5Qcb+niHXihJxwVKcc7p6EXz",
-        tablet: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLF+C47EmbkSlGyJ71yqFy29om1Gs08GZyJN5w7tDRn",
-        "phone-termius": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMbNW3x0Cx04MybxQo5FptZhdjiFJjKITtq67KqrDXAN+jepJwdIG6TUJzuJddztuYIsvPAoQPpiQVSPyC8y51Y=",
-        "tablet-termius": "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8oqpyU3UW9nLzhTFO6AnDvG2Gf/UQGbB0xgtT8JMmr",
+        desktop: [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBaSKoWYuR43fxRPy31P/X/2Ri2hYUZTjKiLBRDoa1F"
+        ],
+        laptop: [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEqeWe+pVp3rTsmHr9v5Qcb+niHXihJxwVKcc7p6EXz"
+        ],
+        tablet: [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8oqpyU3UW9nLzhTFO6AnDvG2Gf/UQGbB0xgtT8JMmr",
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLF+C47EmbkSlGyJ71yqFy29om1Gs08GZyJN5w7tDRn"
+        ],
+        "termius": [
+            "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMbNW3x0Cx04MybxQo5FptZhdjiFJjKITtq67KqrDXAN+jepJwdIG6TUJzuJddztuYIsvPAoQPpiQVSPyC8y51Y=",
+        ]
     },
     others: [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAkdmKF1cYQTW7cfK7TYC5iVBsAg5g3SRJqlqo2NixHdAAAABHNzaDo="
@@ -24,11 +32,13 @@ export function onRequest(context) {
         output += `${SSH_PUBKEYS.others[key]}\n`;
     }
     for (let device in SSH_PUBKEYS.devices) {
-        output += SSH_PUBKEYS.devices[device];
-        if (add_host_metadata) {
-            output += ` ewpratten@ewpratten-${device}\n`;
-        } else {
-            output += "\n";
+        for (let key in SSH_PUBKEYS.devices[device]) {
+            output += SSH_PUBKEYS.devices[device][key];
+            if (add_host_metadata) {
+                output += ` ewpratten@ewpratten-${device}\n`;
+            } else {
+                output += "\n";
+            }
         }
     }
 
